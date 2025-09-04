8ca6d3c345cab388b76d739a27866161cbe7f83c
muhammad talal <talalilyas11@gmail.com>
2025-09-04 16:40:33 +0500

    the button is completed and deployed

--- src/Button123.js
+++ src/Button123.js
@@ -1,4 +1,4 @@
-import { Button, Flex, Tooltip,Row,Col,ConfigProvider } from "antd";
+import { Button, Flex, Tooltip, Row, Col, ConfigProvider } from "antd";
 import Priceupdate from "./Priceupdate";
 
 import { useNavigate } from "react-router-dom";
@@ -9,34 +9,38 @@ export default function Button1() {
   function handleClick() {
     navigate("/priceupdate");
   }
-  function click( ) {
-
-
-    navigate ("/previoustransaction")
+  function click() {
+    navigate("/previoustransaction");
   }
 
   return (
     <ConfigProvider
       theme={{
         token: {
-          colorPrimary: "black", 
+          colorPrimary: "black",
         },
       }}
     >
-     <Row justify="end">
-      <Col>
-    <Button type="primary" onClick={handleClick}    justify="end"style={{marginTop:"4px" ,marginRight:"5px"}}>
-     Add wallet
-    </Button>
-       <Button type="primary" onClick={click}    justify="end"style={{marginTop:"4px"}}>
-     previous transaction
-    </Button>
-    
-    
-    
-
-    </Col>
-    </Row>
+      <Row justify="end">
+        <Col>
+          <Button
+            type="primary"
+            onClick={handleClick}
+            justify="end"
+            style={{ marginTop: "4px", marginRight: "5px" }}
+          >
+            Add wallet
+          </Button>
+          <Button
+            type="primary"
+            onClick={click}
+            justify="end"
+            style={{ marginTop: "4px" }}
+          >
+            previous transaction
+          </Button>
+        </Col>
+      </Row>
     </ConfigProvider>
   );
 }
