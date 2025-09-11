6f124a34e3d3910d7b9d275bd6a10106bc336d04
muhammad talal <talalilyas11@gmail.com>
2025-09-11 18:05:16 +0500

    fix:the route

--- src/App.js
+++ src/App.js
@@ -2,79 +2,16 @@ import { Button } from "antd";
 import "./App.css";
 import { useCallback, useState } from "react";
 import Button1 from "./Button123";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 function App() {
-  // const [count, setCount] = useState(0);
-
-  // const [text, setText] = useState("");
-
-//   const [reset ,setreset] =useState('');
-// // WRONG
-              
-
-
-
-
-// // CORR     
-// let names = ['talal', 'tahir', 'sadia'];
-//   const handleClicks = useCallback (() => {
-//     setCount((prevCount) => prevCount + 1);
-//     setCount((prevCount) => prevCount + 1);
-//     setCount((prevCount) => prevCount + 1);
-//   },[]);
-
-//   const handleChange = (event) => {
-//     setText(event.target.value);
-//      setreset (event.target.reset )
-//   };
-
-// const handleClick  =useCallback(( ) => {
-
-
-//    setreset (event.target.reset )
-// },[]
-// )
-
-// function  printname(name) {
-
-// console.log(`name,${names}`)
-// }
-  
-
-// names.forEach(printname)
-
-  // return (
-//     <div className="App">
-//       <button onClick={handleClick}>{count}</button>
-
-//       <input value={text} onChange={handleChange} type="text" />
-
-//       <p>{text}</p>
-
-
-//     <butoon onClick={handleClick} > 
-// A
-//       restA
-//     </butoon>
-
-
-
-//     <button  onClick={handleClicks} >
-
-//     </button>
-// //     </div>
-//   );
-
-
-
-
-
-return (
-
-
-  <Button1/>
-
-  
-)
+  return (
+    <>
+    
+      <Header/>
+      <Sidebar/>
+    </>
+  );
 }
 
 export default App;
