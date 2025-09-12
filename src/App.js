91cc4cb6055fe2103ab6eea95daa36b6208e8ba7
muhammad talal <talalilyas11@gmail.com>
2025-09-12 17:51:21 +0500

    fix the issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { Button } from "antd";
 import "./App.css";
-import { useCallback, useState } from "react";
-import Button1 from "./Button123";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 function App() {
   return (
     <>
-    
-      <Header/>
-      <Sidebar/>
+      <Header />
+      <Sidebar />
     </>
   );
 }
