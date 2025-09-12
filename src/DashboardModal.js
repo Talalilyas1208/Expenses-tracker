e2030568e1c0eed1bbb4c46b7dd0f044c743cd3b
muhammad talal <talalilyas11@gmail.com>
2025-09-12 17:52:13 +0500

    Added :modal in the page and working on more update

---
+++ src/DashboardModal.js
@@ -0,0 +1,36 @@
+    import React, { useState } from "react";
+    import { Modal, Segmented, ConfigProvider, theme} from "antd";
+    import Expense from "./Expense";
+    import Income from "./Income";
+    import Transfer from "./Transfer";
+
+    export default function DashboardModal({ open, onClose }) {
+    const [currentView, setCurrentView] = useState("Expense");
+
+    const contentMap = {
+        Expense: <Expense />,
+        Income: <Income />,
+        Transfer: <Transfer />,
+    };
+
+    return (
+        <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
+        <Modal
+            title={
+            <Segmented
+                size="middle"
+                options={["Expense", "Income", "Transfer"]}
+                value={currentView}
+                onChange={(val) => setCurrentView(val)}
+            />
+            }
+            open={open}
+            onCancel={onClose}
+            footer={null}
+            width={500}
+        >
+            {contentMap[currentView]}
+        </Modal>
+        </ConfigProvider>
+    );
+    }
