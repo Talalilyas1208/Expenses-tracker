862c34100c5d0bf7f2e790df56fc666186617852
muhammad talal <talalilyas11@gmail.com>
2025-09-16 16:53:14 +0500

    updates: in buttons

--- src/Button123.js
+++ src/Button123.js
@@ -1,36 +1,36 @@
-import { Button, Row, Col, ConfigProvider } from "antd";
-import Header from "./Header";
+// import { Button, Row, Col, ConfigProvider } from "antd";
+// import Header from "./Header";
 
-import { useNavigate } from "react-router-dom";
-import TotalSummary from "./TotalSummary";
+// import { useNavigate } from "react-router-dom";
+// import TotalSummary from "./TotalSummary";
 
-export default function Button1() {
-  const navigate = useNavigate();
+// export default function Button1() {
+//   const navigate = useNavigate();
 
-  function handleClick() {
-    navigate("/priceupdate");
-  }
-  function click() {
-    navigate("/previoustransaction");
-  }
+//   function handleClick() {
+//     navigate("/priceupdate");
+//   }
+//   function click() {
+//     navigate("/previoustransaction");
+//   }
 
-  return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "black",
-        },
-      }}
-    >
-      <Row justify="end">
-        <Col>
+//   return (
+//     <ConfigProvider
+//       theme={{
+//         token: {
+//           colorPrimary: "black",
+//         },
+//       }}
+//     >
+//       <Row justify="end">
+//         <Col>
 
-        <Header
+//         <Header
          
-          />
-          <TotalSummary/> 
-        </Col>
-      </Row>
-    </ConfigProvider>
-  );
-}
+//           />
+//           <TotalSummary/> 
+//         </Col>
+//       </Row>
+//     </ConfigProvider>
+//   );
+// }
