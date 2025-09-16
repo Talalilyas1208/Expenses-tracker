0574ba0076f7750987e9bebc8b95b636a785b7dd
muhammad talal <talalilyas11@gmail.com>
2025-09-16 18:21:23 +0500

    progress in Wallets

--- src/Wallets.js
+++ src/Wallets.js
@@ -1,3 +1,40 @@
-export default function Wallets ( ) {
+import useFetch from "./Hooks/hookfetchdata";
+import { Row, Col, Table, Spin } from "antd";
 
+export default function Wallets() {
+  const { data, loading } = useFetch("http://localhost:8080/wallets");
+
+  const columns = [
+    {
+      title: "Transaction",
+      dataIndex: "label",
+      key: "label",
+    },
+    {
+      title: "Amount",
+      dataIndex: "transaction_amount",
+      key: "amount",
+      render: (value) => `$${value}`,
+    },
+  ];
+
+  if (loading) {
+    return <Spin tip="Loading..." />;
+  }
+
+  if (!data) {
+    return <div>Error loading wallets</div>;
+  }
+
+  return (
+    <Row>
+      <Col lg={10}>
+        <Table
+          dataSource={data}
+          columns={columns}
+        
+        />
+      </Col>
+    </Row>
+  );
 }
