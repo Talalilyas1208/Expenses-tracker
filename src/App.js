bba475edfc11837ec7017951ae90306381a2e0b4
muhammad talal <talalilyas11@gmail.com>
2025-09-17 18:17:55 +0500

    update the with the typgorphy :App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState, useEffect } from "react";
-import { Layout } from "antd";
+import { Layout,Typography } from "antd";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Button23 from "./Button23";
@@ -8,7 +8,7 @@ import DashboardModal from "./DashboardModal";
 import { EntriesContext } from "./EntriesContext";
 
 const { Header, Content } = Layout;
-
+const Text = Typography;
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -32,24 +32,25 @@ export default function App() {
 
   return (
     <EntriesContext.Provider value={{ entries, setEntries }}>
-      <Layout style={{ minHeight: "100vh" }}>
+      <Layout style={{ minHeight: "100vh" ,}}  >
         <Sidebar />
         <Layout>
           <Header
             style={{
-              background: "#fff",
-              padding: "0 24px",
+              background: "#0a0a0aff",
+          
               display: "flex",
               justifyContent: "space-between",
               alignItems: "center",
             }}
           >
             <div className="logo" />
+
             <Button23 onClick={handleButtonClick} />
           </Header>
 
           <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-            <div style={{ padding: 24, background: "#fff", minHeight: "100%" }}>
+            <div style={{ padding: 24, background: "#0f0f0fff", minHeight: "100%" }}>
               <Outlet />
             </div>
           </Content>
