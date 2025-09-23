b474c136862e7df2c340d657fdac6ac15cf878d1
muhammad talal <talalilyas11@gmail.com>
2025-09-23 18:12:55 +0500

    improvmenets in the button compenet

--- src/Compenets/Buttons.js
+++ src/Compenets/Buttons.js
@@ -1,22 +1,18 @@
-import { Button } from "antd"
-
-
-import { SaveOutlined} from "@ant-design/icons";
-
-
-export default function Buttons({...props}) {
- 
-const {size,text ,icon ,handlesave} = {...props}
+import { Button } from "antd";
 
 
 
+export default function Buttons({ ...props }) {
+  const { size, text, icon, handlesave } = { ...props };
 
-
-    return (
-
-
-      <Button size={size} style={{width:"100%"}} icon={icon} onClick={handlesave}>
-        {text}
-      </Button>
-    )
+  return (
+    <Button
+      size={size}
+      style={{ width: "100%" }}
+      icon={icon}
+      onClick={handlesave}
+    >
+      {text}
+    </Button>
+  );
 }
