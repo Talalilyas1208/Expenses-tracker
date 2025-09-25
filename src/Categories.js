5b9266e0c1a7911fa9db4eecb79b161b054f95b4
muhammad talal <talalilyas11@gmail.com>
2025-09-25 18:02:11 +0500

    In progress : the categories opiton still on the progress

--- src/Categories.js
+++ src/Categories.js
@@ -1,8 +1,55 @@
-export default  function categories () {
+import { Row, Col, Card, Typography, ConfigProvider, Button, Space } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
+export default function () {
+  const { Text } = Typography;
 
+  const handleAddClick = () => {
+    console.log("Plus button clicked!");
+   
+  };
 
-
-
-    
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorBgContainer: "black",
+          colorText: "white",
+          colorTextSecondary: "#bfbfbf",
+          colorPrimary: "#52c41a",
+        },
+        components: {
+          Card: {
+            colorBorderSecondary: "#434343",
+            colorBorder: "#434343",
+          },
+        },
+      }}
+    >
+      <Row>
+        <Col lg={5} xs={24} s={22}>
+          <Card>
+            <Space
+              align="center"
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: "rgba(255,255,255,0.6)" }}>
+                Limits by categories
+              </Text>
+              <Button
+                type="text"
+                icon={<PlusOutlined />}
+                onClick={handleAddClick}
+                style={{ color: "#52c41a" }}
+              />
+            </Space>
+          </Card>
+        </Col>
+      </Row>
+    </ConfigProvider>
+  );
 }
